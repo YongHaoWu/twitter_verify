9a2357635d04aeb72eb40454f627182c5f518e77
yonghao.hyh <yonghao.hyh@alibaba-inc.com>
2018-11-15 17:18:13 +0800

    Add config test.

--- config/config.exs
+++ config/config.exs
@@ -28,3 +28,5 @@ use Mix.Config
 # here (which is why it is important to import them last).
 #
 #     import_config "#{Mix.env()}.exs"
+config :twitter_verify, consumer_key: :test
+config :twitter_verify, consumer_secret: :test
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule TwitterVerify.MixProject do
     [
       app: :twitter_verify,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.4",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
